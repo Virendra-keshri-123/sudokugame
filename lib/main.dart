818783c976a6b11b886d09550cb3e9a506acf486
Virendra keshri <virendrakeshri@playsimple.in>
2024-04-08 16:42:06 +0530

    day4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:sudoku/screens/home_page.dart';
 import 'package:sudoku/screens/login_page.dart';
-
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: '/home',
+      initialRoute: '/login', 
       routes: {
         "/":(context) =>HomePage(),
         "/login":(context)=>LoginPage(),
          "/home":(context) =>HomePage(),
-
         // "/login":(context) => Logi nPage(), 
       },
     );
   }
 }
- 
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -9,15 +9,18 @@ class HomePage extends StatelessWidget {
     return Scaffold( 
       appBar: AppBar(
         title:Text("Welcome to the sudoku game"
+         
         ,
         style: TextStyle(
               color: Colors.yellow,
                // Change this color to your desired color
-            ),),
+            ),
+            ),
       ),
       body: Center(  
         child: Container(
           child: Text('Welcome to $day days of flutter'),
+          
         ),
       ),
       drawer:Drawer(
